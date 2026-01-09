f442974a745f1ae57ef1309db164b3e82cfae1bb
ChiriKamau <chiri.levisk@gmail.com>
2026-01-09 12:57:12 +0300

    Please work

--- code/uart.py
+++ code/uart.py
@@ -1,21 +1,10 @@
 import serial
-import time
 
-# /dev/serial0 is the default UART for GPIO pins 8 and 10
-ser = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
 
-print("Waiting for Pico...")
+print("Listening...")
 
-try:
-    while True:
-        if ser.in_waiting > 0:
-            # Read the line from Pico
-            line = ser.readline().decode('utf-8').rstrip()
-            print(f"Pico sent: {line}")
-            
-            # Send a confirmation back
-            ser.write("Pico, I heard you!\n".encode('utf-8'))
-            
-except KeyboardInterrupt:
-    print("Closing connection.")
-    ser.close()
+while True:
+    line = ser.readline().decode().strip()
+    if line:
+        print("Received:", line)
